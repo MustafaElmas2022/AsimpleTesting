29fa189412e0f43a8a3d9e5b33e4c89696f9f4f0
MustafaElmas2022 <mustafaelmas2019@gmail.com>
2022-04-28 13:18:20 +0200

    added to Test Case 5

--- src/test/java/com/eurotech/Ödevler/SauceDemoTest.java
+++ src/test/java/com/eurotech/Ödevler/SauceDemoTest.java
@@ -32,11 +32,11 @@ public class SauceDemoTest {
     @Test
     public void test1() throws InterruptedException {
         driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@id=\"user-name\"]")).sendKeys("standard_user");
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("secret_sauce");
         Thread.sleep(2000);
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
+        WebElement element = driver.findElement(By.xpath("//input[@id=\"login-button\"]"));
         element.click();
         //driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
 
@@ -51,22 +51,22 @@ public class SauceDemoTest {
     //Test Case 2 - Product Page Function
     @Test
     public void test2() throws InterruptedException {
-        driver.get(" https://www.saucedemo.com/");
-        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.xpath("//input[@id=\"user-name\"]")).sendKeys("standard_user");
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("secret_sauce");
         Thread.sleep(2000);
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
+        WebElement element = driver.findElement(By.xpath("//input[@id=\"login-button\"]"));
         element.click();
         Thread.sleep(2000);
-        WebElement product = driver.findElement(By.xpath("//*[@id=\"item_5_title_link\"]/div"));
+        WebElement product = driver.findElement(By.xpath("(//div[@class=\"inventory_item_name\"])[4]"));
         product.click();
-        String actualResualt = driver.findElement(By.xpath("//*[@id=\"inventory_item_container\"]/div/div/div[2]/div[1]")).getText();
+        String actualResualt = driver.findElement(By.xpath("//div[@class='inventory_details_name large_size']")).getText();
         System.out.println("actualResualt = " + actualResualt);
         String expectedResult = "Sauce Labs Fleece Jacket";
         Assert.assertEquals(actualResualt,expectedResult);
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"back-to-products\"]")).click();
+        driver.findElement(By.xpath("//button[@id='back-to-products']")).click();
 
         System.out.println("TO VERIFY EXPECTED CURRENT URL:" + driver.getCurrentUrl());
         Assert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/inventory.html"));
@@ -75,51 +75,55 @@ public class SauceDemoTest {
     //Test Case 3 - Add to Cart Function
     @Test
     public void test3() throws InterruptedException {
-        driver.get(" https://www.saucedemo.com/");
-        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.xpath("//input[@id=\"user-name\"]")).sendKeys("standard_user");
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("secret_sauce");
         Thread.sleep(2000);
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
+        WebElement element = driver.findElement(By.xpath("//input[@id=\"login-button\"]"));
         element.click();
         Thread.sleep(3000);
         WebElement product1 = driver.findElement(By.id("add-to-cart-sauce-labs-onesie"));
         product1.click();
-        WebElement productText = driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a/span"));
+        WebElement productText = driver.findElement(By.xpath("//div[@id='shopping_cart_container']/a/span"));
         System.out.println("productText.getText() = " + productText.getText());
-        Assert.assertTrue(productText.isDisplayed());
+
         Thread.sleep(3000);
         WebElement product2 = driver.findElement(By.id("remove-sauce-labs-onesie"));
         product2.click();
         Thread.sleep(3000);
-        WebElement productText1 = driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a"));
-        System.out.println("productText1.getText() = " + productText1.getText());
-        Assert.assertTrue(productText1.isDisplayed());
+        String productText1 = driver.findElement(By.xpath("//div[@id=\"shopping_cart_container\"]/a")).getText();
+        System.out.println("productText1.getText() = " + productText1);
+        Assert.assertEquals(productText1,"");
+
     }
 
     // Test Case 4 - Product Display Order On Page
     @Test
     public void test4() throws InterruptedException {
 
-        driver.get(" https://www.saucedemo.com/");
-        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.xpath("//input[@id=\"user-name\"]")).sendKeys("standard_user");
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("secret_sauce");
         Thread.sleep(2000);
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
+        WebElement element = driver.findElement(By.xpath("//input[@id=\"login-button\"]"));
         element.click();
         Thread.sleep(3000);
-        WebElement AdanZye = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select/option[1]"));
+        WebElement AdanZye = driver.findElement(By.xpath("//select[@class='product_sort_container']/../span"));
         System.out.println("AdanZye.getText() = " + AdanZye.getText());
-        Assert.assertTrue(AdanZye.getText().contains("Name (A to Z)"));
+        Assert.assertTrue(AdanZye.getText().contains("NAME (A TO Z)"));
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select")).click();
+        driver.findElement(By.xpath("//select[@class='product_sort_container']")).click();
         Thread.sleep(2000);
-        WebElement lowToHigh = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select/option[3]"));
+        WebElement lowToHigh = driver.findElement(By.xpath("//select[@class='product_sort_container']/option[3]"));
         lowToHigh.click();
         Thread.sleep(2000);
-        String text = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select/option[3]")).getText();
+        String text = driver.findElement(By.xpath("//select[@class='product_sort_container']/../span")).getText();
         System.out.println("text = " + text);
+        String expectedResult = "PRICE (LOW TO HIGH)";
+        Assert.assertEquals(text, expectedResult);
+
 
 
     }
@@ -127,12 +131,12 @@ public class SauceDemoTest {
     // Test Case 5 - Shopping Function
     @Test
     public void test5() throws InterruptedException {
-        driver.get(" https://www.saucedemo.com/");
-        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.xpath("//input[@id=\"user-name\"]")).sendKeys("standard_user");
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("secret_sauce");
         Thread.sleep(2000);
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
+        WebElement element = driver.findElement(By.xpath("//input[@id=\"login-button\"]"));
         element.click();
         Thread.sleep(3000);
         WebElement ürün = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
@@ -155,11 +159,11 @@ public class SauceDemoTest {
 
             String orjinalFiyat = driver.findElement(By.xpath("//div[@class=\"inventory_item_price\"]")).getText();
             System.out.println("orjinalFiyat = " + orjinalFiyat);
-            String totalFiyat = driver.findElement(By.xpath("//div[@class='summary_total_label']")).getText();
-            System.out.println("totalFiyat = " + totalFiyat);
-        Assert.assertFalse(orjinalFiyat.contains(totalFiyat), "Check the item total price is same with original price of product");
+            String itemTotalFiyat = driver.findElement(By.xpath("//div[@class='summary_subtotal_label']")).getText();
+            System.out.println("totalFiyat = " + itemTotalFiyat);
+        Assert.assertTrue(itemTotalFiyat.contains(orjinalFiyat), "Check the item total price is same with original price of product");
         boolean teyit = false;
-        if (orjinalFiyat.equals(totalFiyat)){
+        if (orjinalFiyat.equals(itemTotalFiyat)){
             System.out.println("Teyit 1 : " +teyit);
         }
         else {
